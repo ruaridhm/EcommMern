0c492112ede07f615347870699398b89bdc8ba11
ruaridhm <ruaridh.mcguckin@gmail.com>
2020-11-02 12:05:36 +0000

    quantity selector, add to cart button

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import {
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_FAIL,
-    PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_REQUEST,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_FAIL,
-} from '../constants/productContants.js';
+} from '../constants/productConstants.js';
 
 export const listProducts = () => async (dispatch) => {
   try {
@@ -30,21 +30,22 @@ export const listProducts = () => async (dispatch) => {
 };
 
 export const listProductDetails = (id) => async (dispatch) => {
-    try {
-      dispatch({ type: PRODUCT_DETAILS_REQUEST });
-  
-      const { data } = await axios.get(`/api/products/${id}`);
-  
-      dispatch({
-        type: PRODUCT_DETAILS_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: PRODUCT_DETAILS_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
