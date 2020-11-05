97d174d7e03a30d3ad0f66740047955a6200e0ed
ruaridhm <ruaridh.mcguckin@gmail.com>
2020-11-05 23:23:45 +0000

    get order by id controller and route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,13 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
-// router
-//   .route('/profile')
-//   .get(protect, getUserProfile)
-//   .put(protect, updateUserProfile);
-// router.route('/').post(registerUser);
-
+router.route('/:id').get(protect, getOrderById);
 export default router;
